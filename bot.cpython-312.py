9f329e20631336e74f5bb5c87e3dc28ec01df4cf
FuturoIA2025 <reinaldobenavides89@gmail.com>
2025-03-27 19:16:53 +0000

    bot.cpytho-3

---
+++ bot.cpython-312.py
@@ -0,0 +1,6 @@
+# uncompyle6 version 3.9.2
+# Python bytecode version base 3.12.0 (3531)
+# Decompiled from: Python 3.12.1 (main, Dec 12 2024, 22:30:56) [GCC 9.4.0]
+# Embedded file name: /workspaces/Futuro-IA/bot.py
+# Compiled at: 2025-03-20 21:18:00
+# Size of source mod 2**32: 298 bytes
