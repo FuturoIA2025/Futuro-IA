c8251723aca14a5c9e97cc900f7a7785515b98f7
FuturoIA2025 <reinaldobenavides89@gmail.com>
2025-03-28 20:37:37 +0000

    Confg.py

--- Confg.py
+++ Confg.py
@@ -1,13 +1,13 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv  # Importa load_dotenv
 # Cargar variables de entorno
 load_dotenv()
 # Obtener los tokens
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+# TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
 # Validar que los tokens existen
-if not TELEGRAM_TOKEN:
+# if not TELEGRAM_TOKEN:
 raise ValueError("Error: TELEGRAM_TOKEN no está configurado en el entorno.")
 if not HUGGINGFACE_TOKEN:
 raise ValueError("Error: HUGGINGFACE_TOKEN no está configurado en el entorno.")
-print("✅ Configuración cargada correctamente.")   
+print("✅ Configuración cargada correctamente.")  
