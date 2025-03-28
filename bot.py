6bf5840d7cdac1579f78f9752476b97d7cf4032f
FuturoIA2025 <reinaldobenavides89@gmail.com>
2025-03-28 21:34:23 +0000

    bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
-import requests  
 from config import TELEGRAM_TOKEN, CHAT_ID
-def enviar_mensaje(mensaje):  
-url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-datos = {"chat_id": CHAT_ID, "text": mensaje}  
-response = requests.post(url, data=datos)  
-response.raise_for_status()  # Verifica si hay errores  
-if __name__ == "__main__":  
+print("TOKEN:", TELEGRAM_TOKEN)  # Para ver si se importa correctamente
+def enviar_mensaje(mensaje):
+ url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+datos = {"chat_id": CHAT_ID, "text": mensaje}
+response = requests.post(url, data=datos)
+response.raise_for_status()
+if __name__ == "__main__":
  enviar_mensaje("¡Hola desde mi bot de Telegram!")
