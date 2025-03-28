9573fbe8b536e7139ebe0956fd00b01f726dfcba
FuturoIA2025 <reinaldobenavides89@gmail.com>
2025-03-28 21:12:51 +0000

    Confg.py

--- Confg.py
+++ Confg.py
@@ -3,11 +3,14 @@ from dotenv import load_dotenv  # Importa load_dotenv
 # Cargar variables de entorno
 load_dotenv()
 # Obtener los tokens
-# TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 # Validar que los tokens existen
-# if not TELEGRAM_TOKEN:
+if not TELEGRAM_TOKEN:
 raise ValueError("Error: TELEGRAM_TOKEN no está configurado en el entorno.")
 if not HUGGINGFACE_TOKEN:
 raise ValueError("Error: HUGGINGFACE_TOKEN no está configurado en el entorno.")
-print("✅ Configuración cargada correctamente.")  
+if not CHAT_ID:
+raise ValueError("Error: TELEGRAM_CHAT_ID no está configurado en el entorno.")
+print("✅ Configuración cargada correctamente.")
