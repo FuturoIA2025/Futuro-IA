62a2a88f0a824c8fb86b7a85e874c9f6210e3acc
FuturoIA2025 <reinaldobenavides89@gmail.com>
2025-03-29 22:38:07 +0000

    Confg.py

--- Confg.py
+++ Confg.py
@@ -1,10 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-# Cargar las variables desde .env
+# Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
-# Obtener los valores de las variables
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
+# Cargar las configuraciones con los nombres correctos
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+HUGGINGFACE_API_KEY = os.getenv('HUGGINGFACE_TOKEN')
