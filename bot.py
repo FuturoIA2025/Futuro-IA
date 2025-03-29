a0f88aa4ae5f3cc56753bd2ac7b8118ff4d9bd4a
FuturoIA2025 <reinaldobenavides89@gmail.com>
2025-03-29 17:07:47 -0500

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,5 @@
-import sys
-sys.path.append(".")  # Asegura que Python busque en la carpeta actual
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, HUGGINGFACE_TOKEN
 
-from config import TELEGRAM_TOKEN, CHAT_ID 
-
-print("TOKEN:", TELEGRAM_TOKEN)  # Verifica si se importa correctamente
-
-def enviar_mensaje(mensaje):  
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"  
-    datos = {"chat_id": CHAT_ID, "text": mensaje}  
-    response = requests.post(url, json=datos)  # Usa `json=` en vez de `data=`  
-    response.raise_for_status()  
-
-if __name__ == "__main__":  
-    enviar_mensaje("¡Hola desde mi bot de Telegram!")
+print("Token de Telegram:", TELEGRAM_TOKEN)
+print("Chat ID de Telegram:", TELEGRAM_CHAT_ID)
+print("Token de Hugging Face:", HUGGINGFACE_TOKEN)
