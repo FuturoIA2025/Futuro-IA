a2e9d0e71f1c34eb4877610e6a0da0b9216628ee
FuturoIA2025 <reinaldobenavides89@gmail.com>
2025-03-30 18:55:38 +0000

    config.py

--- Config.py
+++ Config.py
@@ -8,5 +8,6 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
 
+# Verificar que todas las variables estén definidas
 if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID or not HUGGINGFACE_TOKEN:
  raise ValueError("Faltan variables en el archivo .env. Verifica que TELEGRAM_TOKEN, TELEGRAM_CHAT_ID y HUGGINGFACE_TOKEN estén definidos.")
