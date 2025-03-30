d53d55bb52a7b3cf97635ddfa3fd4a700e8824ed
FuturoIA2025 <reinaldobenavides89@gmail.com>
2025-03-30 18:30:27 +0000

    config.py

--- Config.py
+++ Config.py
@@ -1,9 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# Cargar las variables de entorno desde el archivo .env
+# Cargar las variables del archivo .env
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-HUGGINGFACE_TOKEN = os.getenv('HUGGINGFACE_TOKEN')
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
+
+if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID or not HUGGINGFACE_TOKEN:
+ raise ValueError("Faltan variables en el archivo .env. Verifica que TELEGRAM_TOKEN, TELEGRAM_CHAT_ID y HUGGINGFACE_TOKEN estén definidos.")
